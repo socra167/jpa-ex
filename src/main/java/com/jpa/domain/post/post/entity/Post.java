99dd1a268145cdac5ae1b169ca04173e45e1857a
socra167 <socra167@gmail.com>
2025-01-20 10:26:13 +0900

    :sparkles: feat: `id` 타입을 `Long`으로 변경

--- src/main/java/com/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/jpa/domain/post/post/entity/Post.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Id;
 public class Post {
 	@Id // PRIMARY KEY
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
-	private long id;
+	private Long id; // long보다 null이 가능한 Long이 적합하다. long은 0으로 초기화되는데 값이 없는 것인지, 실제 값이 0인지 판별할 수 없다.
 	private LocalDateTime createdDate;
 	private LocalDateTime modifiedDate;
 	@Column(length = 100)
