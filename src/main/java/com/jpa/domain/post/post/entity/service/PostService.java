90547757ae3eb2ce0a6f1df98298ac02659a2154
socra167 <socra167@gmail.com>
2025-01-20 14:07:00 +0900

    :sparkles: feat: `Post` entity 수정 기능

--- src/main/java/com/jpa/domain/post/post/entity/service/PostService.java
+++ src/main/java/com/jpa/domain/post/post/entity/service/PostService.java
@@ -1,6 +1,9 @@
 package com.jpa.domain.post.post.entity.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jpa.domain.post.post.entity.Post;
 import com.jpa.domain.post.post.repository.PostRepository;
@@ -21,7 +24,19 @@ public class PostService {
 		return postRepository.save(post); // repository에서 save 하면 기본적으로 객체를 반환한다
 	}
 
+	@Transactional
+	public Post modify(Post post1, String title, String body) {
+		Post post = postRepository.findById(post1.getId()).get();
+		post.setTitle(title);
+		post.setBody(body);
+		return post;
+	}
+
 	public long count() {
 		return postRepository.count();
+	}
+
+	public Optional<Post> findById(long id) {
+		return postRepository.findById(id);
 	}
 }
--- src/main/java/com/jpa/global/BaseInitData.java
+++ src/main/java/com/jpa/global/BaseInitData.java
@@ -3,6 +3,7 @@ package com.jpa.global;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 import com.jpa.domain.post.post.entity.Post;
 import com.jpa.domain.post.post.entity.service.PostService;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 public class BaseInitData {
 	private final PostService postService;
 
+	@Order(1)
 	@Bean
 	public ApplicationRunner applicationRunner() {
 		return args -> {
@@ -24,6 +26,17 @@ public class BaseInitData {
 			Post p1 = postService.write("title1", "body1");
 			Post p2 = postService.write("title2", "body2");
 			Post p3 = postService.write("title3", "body3");
+		};
+	}
+
+	// 설정을 하지 않으면 어떤 ApplicationRunner가 먼저 실행될지 보장되지 않는다
+	@Order(2) // 빈 생성의 순서를 설정할 수 있다
+	@Bean
+	public ApplicationRunner applicationRunner2() {
+		return args -> {
+			Post post = postService.findById(1L).get();
+			Thread.sleep(1000);
+			postService.modify(post, "new title", "new body");
 		};
 	}
 }
