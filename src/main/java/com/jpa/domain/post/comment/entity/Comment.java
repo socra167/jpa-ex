e9569c7301e96bdef1f7bae4bc78cbfd2c5ab2ed
socra167 <socra167@gmail.com>
2025-01-21 11:05:51 +0900

    :wrench: chore: `Comment`에서 `Post` LAZY fetch하도록 변경

--- src/main/java/com/jpa/domain/post/comment/entity/Comment.java
+++ src/main/java/com/jpa/domain/post/comment/entity/Comment.java
@@ -11,6 +11,7 @@ import com.jpa.domain.post.post.entity.Post;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -48,7 +49,7 @@ public class Comment {
 	@Column(columnDefinition = "TEXT")
 	private String body;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "post_id")
 	private Post post;
 }
