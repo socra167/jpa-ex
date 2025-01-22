5c656d93c7881996ce0605fe80720f50659ff697
socra167 <socra167@gmail.com>
2025-01-22 12:10:44 +0900

    :wrench: chore: `application.yml` 환경별 profile 설정

---
+++ src/test/java/com/jpa/domain/post/post/service/PostServiceTest.java
@@ -0,0 +1,15 @@
+package com.jpa.domain.post.post.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class PostServiceTest {
+
+	@Test
+	void test() {
+		System.out.println("123");
+	}
+}
